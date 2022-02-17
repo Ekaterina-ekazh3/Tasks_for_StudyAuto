4c47df7b311675d45e44ca5ec305c264228537f6
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-02-17 21:08:42 +0300

    Добавлен isinstance и исключения

--- Task_1_factorial.py
+++ Task_1_factorial.py
@@ -1,20 +1,16 @@
 # Задание:
 # Необходимо написать функцию, которая получает на вход натуральное число N, а возвращает число нулей на конце N!
-def count_zeros(n):
-    zeros = 0  # стартовое значение счетчика нулей
-    m = 1  # стартовая степень для 5
-    while (n // (5 ** m)) > 0:
-        tmp_count = (n // (5 ** m))
-        m += 1
-        zeros += tmp_count
-    return zeros
-
-
-#  n = int(input())  # получение ввода натурального числа
-n = input()
-is_num = n.isdigit()
-try:
-    n = int(n)
-    print(count_zeros(n))  # вычисление количества нулей факториала (вызов функции) и вывод на печать
-except ValueError:
-    print("Вы ввели не число!")
+def count_zeros(n: int) -> int:
+    if isinstance(n, int) is True:
+        if n >= 0:
+            zeros = 0  # стартовое значение счетчика нулей
+            m = 1  # стартовая степень для 5
+            while (n // (5 ** m)) > 0:
+                tmp_count = n // (5 ** m)
+                m += 1
+                zeros += tmp_count
+            return zeros
+        else:
+            raise ValueError("Вы ввели отрицательное число!")
+    else:
+        raise TypeError("Вы ввели не число!")
