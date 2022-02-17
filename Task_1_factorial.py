414096e6ff529cf564fe74682846dcb8a2d52a1c
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-02-17 21:26:03 +0300

    reorder and else-delete

--- Task_1_factorial.py
+++ Task_1_factorial.py
@@ -1,16 +1,14 @@
 # Задание:
 # Необходимо написать функцию, которая получает на вход натуральное число N, а возвращает число нулей на конце N!
 def count_zeros(n: int) -> int:
-    if isinstance(n, int) is True:
-        if n >= 0:
-            zeros = 0  # стартовое значение счетчика нулей
-            m = 1  # стартовая степень для 5
-            while (n // (5 ** m)) > 0:
-                tmp_count = n // (5 ** m)
-                m += 1
-                zeros += tmp_count
-            return zeros
-        else:
-            raise ValueError("Вы ввели отрицательное число!")
-    else:
+    if not isinstance(n, int):
         raise TypeError("Вы ввели не число!")
+    if n < 0:
+        raise ValueError("Вы ввели отрицательное число!")
+    zeros = 0  # стартовое значение счетчика нулей
+    m = 1  # стартовая степень для 5
+    while (n // (5 ** m)) > 0:
+        tmp_count = n // (5 ** m)
+        m += 1
+        zeros += tmp_count
+    return zeros
