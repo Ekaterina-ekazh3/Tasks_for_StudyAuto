b71980fcb39d80a6a5e47c4d6ef11f544b8a0dac
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-04-18 13:02:39 +0300

    convert to function-2, optimisation, bugfixing

--- Task_2_poker.py
+++ Task_2_poker.py
@@ -6,8 +6,8 @@
 #  2,3,4,5,6,7,8,9,10,11,12,13,14 - 2,3,4,5,6,7,8,9,10,J,Q,K,A соответственно
 #  'H', 'S', 'C', 'D' - Hearts, Spades, Clubs, Diamonds соответственно
 
-import copy
-import random
+
+# import random
 from itertools import combinations
 from collections import Counter
 
@@ -26,7 +26,115 @@ def define_winner_hand(scs: list) -> tuple:
         shnd = sorted(hnd, key=lambda crd: crd[1])
         ordered_all_combo_in_hand_by_suits.append(shnd)
 
-    # Ранжирование рук (создание списка с очками для всех комбинаций в руке)
+    hands_scores = scoring_for_a_combination(ordered_all_combo_in_hand_by_suits)
+
+    # Найти максимум в полученном списке оцененных рук и количество рук с максимумом очков
+    max_score = max(hands_scores)
+
+    ind_max = []
+    count_ind = 0
+    for s in hands_scores:
+        if s == max_score:
+            ind_max.append(count_ind)
+        count_ind += 1
+
+    # Собираем список рук с выигрышными по баллу комбинациями и сортируем по номиналу
+    hand_max = []
+    for i in ind_max:
+        hand_max.append(sorted(all_combo_in_hand[i], key=lambda crd: crd[0]))
+
+    # Определяем итоговую выигрышную комбинацию
+    if len(hand_max) == 1:
+        win_hand = hand_max[0]
+
+        return win_hand, max_score
+
+    else:
+        win_hand = ()
+        if max_score == 10:
+            pass
+
+        if max_score == 9:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+
+            for hand in hand_max_without_suit:
+                if 14 in hand and 2 in hand:
+                    hand.insert(0, 14)
+                    hand.pop(5)
+
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 8:
+            pass
+
+        if max_score == 7:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+
+            nominals = []
+            for hand in hand_max_without_suit:
+                count_start = hand.count(hand[0])
+                if count_start == 3:
+                    nominals.append([hand[0], hand[4]])
+                else:
+                    nominals.append([hand[4], hand[0]])
+
+            win_hand = ()
+            for i in range(len(nominals) - 1):
+                if nominals[i][0] == nominals[i + 1][0]:
+                    if nominals[i][1] == nominals[i + 1][1]:
+                        win_hand = hand_max[i]
+                    else:
+                        if nominals[i][1] > nominals[i + 1][1]:
+                            win_hand = hand_max[i]
+                        else:
+                            win_hand = hand_max[i + 1]
+                else:
+                    if nominals[i][0] > nominals[i + 1][0]:
+                        win_hand = hand_max[i]
+                    else:
+                        win_hand = tuple(hand_max[i + 1])
+
+        if max_score == 6:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 5:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+
+            for hand in hand_max_without_suit:
+                if 14 in hand and 2 in hand:
+                    hand.insert(0, 14)
+                    hand.pop(5)
+
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 4:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+
+            for hand in hand_max_without_suit:
+                value_of_three = hand[2]
+                ind = hand_max_without_suit.index(hand)
+                for i in range(3):
+                    hand_max_without_suit[ind].remove(value_of_three)
+
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 3:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 2:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+            find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        if max_score == 1:
+            hand_max_without_suit = clean_of_card_suit(hand_max)
+            win_hand = find_winner_hand(hand_max_without_suit, hand_max, max_score)
+
+        return win_hand, max_score
+
+
+def scoring_for_a_combination(ordered_all_combo_in_hand_by_suits):
     hands_scores = []
 
     for shnd in ordered_all_combo_in_hand_by_suits:
@@ -85,145 +193,35 @@ def define_winner_hand(scs: list) -> tuple:
                         continue
                 else:
                     hands_scores.append(1)  # Highcard
-
     print(hands_scores)
+    return hands_scores
 
-    # Найти максимум в полученном списке оцененных рук и количество рук с максимумом очков
-    max_score = max(hands_scores)
 
-    ind_max = []
-    count_ind = 0
-    for s in hands_scores:
-        if s == max_score:
-            ind_max.append(count_ind)
-        count_ind += 1
+def clean_of_card_suit(hand_max):
+    hand_max_without_suit = []
+    for hand in hand_max:
+        hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
+    return hand_max_without_suit
 
-    # Собираем список рук с выигрышными по баллу комбинациями и сортируем по номиналу
-    hand_max = []
-    for i in ind_max:
-        hand_max.append(sorted(all_combo_in_hand[i], key=lambda crd: crd[0]))
 
-    # Определяем итоговую выигрышную комбинацию
-    if len(hand_max) == 1:
-        win_hand = hand_max[0]
-        return win_hand, max_score
+def find_winner_hand(hand_max_without_suit, hand_max, max_score):
+    if max_score == 9 or max_score == 5:
+        winner = 0
+        winner_hand_without_suit = []
+        for hand in hand_max_without_suit:
+            if hand[4] > winner:
+                winner = hand[4]
+                winner_hand_without_suit = hand
+
+        windex = hand_max_without_suit.index(winner_hand_without_suit)
+        win_hand = hand_max[windex]
+        return win_hand
 
     else:
-        if max_score == 10:
-            pass
-
-        if max_score == 9:
-            winner = 0
-            win_hand = []
-            for hand in hand_max:
-                if hand[4][0] > winner:
-                    winner = hand[4][0]
-                    win_hand = hand
-            return win_hand, max_score
-
-        if max_score == 8:
-            pass
-
-        if max_score == 7:
-            nominals = []
-            for hand in hand_max:
-                count_start = hand.count(hand[0][0])
-                if count_start == 3:
-                    nominals.append([hand[0][0], hand[4][0]])
-                else:
-                    nominals.append([hand[4][0], hand[0][0]])
-
-            if nominals[0][0] == nominals[1][0]:
-                if nominals[0][1] > nominals[1][1]:
-                    win_hand = hand_max[0]
-                else:
-                    win_hand = hand_max[1]
-
-            else:
-                if nominals[0][0] > nominals[1][0]:
-                    win_hand = hand_max[0]
-                else:
-                    win_hand = hand_max[1]
-            return win_hand, max_score
-
-        if max_score == 6:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            winner_hand_without_suit = max(hand_max_without_suit)
-            windex = hand_max_without_suit.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
-
-        if max_score == 5:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            for hand in hand_max_without_suit:
-                if 14 in hand and 2 in hand:
-                    hand.insert(0, 14)
-                    hand.pop(5)
-
-            winner = 0
-            winner_hand_without_suit = 0
-            for hand in hand_max_without_suit:
-                if hand[4] > winner:
-                    winner = hand[4]
-                    winner_hand_without_suit = hand
-
-            windex = hand_max_without_suit.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
-
-        if max_score == 4:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            hand_max_without_three = copy.deepcopy(hand_max_without_suit)
-            print(type(hand_max_without_three))
-            for hand in hand_max_without_suit:
-                value_of_three = hand[2]
-                ind = hand_max_without_suit.index(hand)
-                for i in range(3):
-                    hand_max_without_three[ind].remove(value_of_three)
-
-            winner_hand_without_suit = max(hand_max_without_three)
-            windex = hand_max_without_three.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
-
-        if max_score == 3:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            winner_hand_without_suit = max(hand_max_without_suit)
-            windex = hand_max_without_suit.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
-
-        if max_score == 2:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            winner_hand_without_suit = max(hand_max_without_suit)
-            windex = hand_max_without_suit.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
-
-        if max_score == 1:
-            hand_max_without_suit = []
-            for hand in hand_max:
-                hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-
-            winner_hand_without_suit = max(hand_max_without_suit)
-            windex = hand_max_without_suit.index(winner_hand_without_suit)
-            win_hand = hand_max[windex]
-            return win_hand, max_score
+        winner_hand_without_suit = max(hand_max_without_suit)
+        windex = hand_max_without_suit.index(winner_hand_without_suit)
+        win_hand = hand_max[windex]
+        return win_hand
 
 
 combo_names = {1: 'Highcard', 2: 'Pair', 3: 'Two pairs', 4: 'Three of a kind', 5: 'Straight',
@@ -232,18 +230,25 @@ combo_names = {1: 'Highcard', 2: 'Pair', 3: 'Two pairs', 4: 'Three of a kind', 5
 # seven_cards = [(5, 'S'), (4, 'D'), (6, 'D'), (12, 'C'), (4, 'S'), (7, 'D'), (2, 'D')]
 # seven_cards = [(5, 'S'), (5, 'D'), (3, 'D'), (5, 'C'), (3, 'S'), (7, 'D'), (2, 'D')]
 # seven_cards = [(6, 'C'), (3, 'D'), (14, 'C'), (6, 'D'), (6, 'S'), (9, 'S'), (13, 'C')]
+seven_cards = [(12, 'D'), (13, 'S'), (7, 'S'), (11, 'S'), (14, 'D'), (6, 'C'), (4, 'D')]
 
 #  Создаем колоду с картами (кортеж кортежей)
-all_cards = tuple([(x, y) for x in range(2, 15) for y in ('H', 'S', 'C', 'D')])
+# all_cards = tuple([(x, y) for x in range(2, 15) for y in ('H', 'S', 'C', 'D')])
 
 #  Создаем случайную руку из 7 карт
-seven_cards = random.sample(all_cards, 7)
+# seven_cards = random.sample(all_cards, 7)
 
 winner_hand, maximum_score = define_winner_hand(seven_cards)
 print("Winner:", winner_hand, "-", combo_names.get(maximum_score))
 
-# Сделать чтение набора карт из файла?
+
+# Исправить места вызова функции find_winner_hand
+# Разобраться, нужно или нет возвращать max_score (из функции find_winner_hand вроде бы нет, а из
+# "определения итоговой выигрышной комбинации" - да, ее еще нужно обернуть в функцию.
 # Обернуть код в функцию
-# Предусмотреть возможные экспешены.
 # Некоторые повторяющиеся действия можно бы переписать в функции, и вызывать по необходимости.
-# Переписать Йода-условия на человеческий.
+# Предусмотреть возможные экспешены (isinstance)
+# Добавить проверки типов вводимого и возвращаемого значения для каждой функции
+# Для проверки вводимого значения головной функции возможно есть смысл создать класс (возможно, унаследовать от list),
+# объект которого будет представлять собой список-руку состоящую из таплов-карт
+# (иначе слишком много нужно навернуть условий, чтобы проверить список такой степени вложенности)
