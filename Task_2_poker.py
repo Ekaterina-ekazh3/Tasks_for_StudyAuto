f2cdaf6f7981c3605e976c24d70fd71c74047a56
ekazh3 <Ekaterina.Zhukova@steor.tech>
2022-05-23 04:40:18 +0300

    fix and optimise

--- Task_2_poker.py
+++ Task_2_poker.py
@@ -7,16 +7,52 @@
 #  'H', 'S', 'C', 'D' - Hearts, Spades, Clubs, Diamonds соответственно
 
 
-import random
-from itertools import combinations
 from collections import Counter
+from itertools import combinations
+# import random
 
 
 class SevenCards:
-    list_of_cards = []
+    def __init__(self, k_c=None):
+        self.list_of_cards = k_c or []
 
-    def __init__(self, cards):
-        self.list_of_cards = cards
+        if not SevenCards.check_quantity(k_c=self.list_of_cards, quantity=7):
+            raise RuntimeError(f"Invalid quantity of cards: {len(set(self.list_of_cards))}!")
+
+        if not SevenCards.check_duplicates(k_c=self.list_of_cards):
+            raise RuntimeError(f"Card deck has duplicates: {self.list_of_cards}!")
+
+        if not SevenCards.check_card_attributes(k_c=self.list_of_cards):
+            raise RuntimeError("Invalid card attributes!")
+
+        if not SevenCards.check_nominal(k_c=self.list_of_cards):
+            raise ValueError("Invalid value of card nominal!")
+
+        if not SevenCards.check_suit(k_c=self.list_of_cards):
+            raise ValueError("Invalid value of card suit!")
+
+    @staticmethod
+    def check_quantity(k_c: list, quantity: int) -> bool:
+        return len(set(k_c)) == quantity
+
+    @staticmethod
+    def check_duplicates(k_c: list) -> bool:
+        return True
+
+    @staticmethod
+    def check_card_attributes(k_c: list) -> bool:
+        def check_card(card: tuple) -> bool:
+            return len(card) == 2 and type(card[0]) == int and type(card[1]) == str
+
+        return False not in list(map(check_card, k_c))
+
+    @staticmethod
+    def check_nominal(k_c: list) -> bool:
+        return False not in [True if c[0] in (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14) else False for c in k_c]
+
+    @staticmethod
+    def check_suit(k_c: list) -> bool:
+        return False not in [True if c[1] in ('H', 'S', 'C', 'D') else False for c in k_c]
 
 
 def define_winner_hand(scs: SevenCards) -> tuple:
@@ -25,107 +61,77 @@ def define_winner_hand(scs: SevenCards) -> tuple:
     if not scs.list_of_cards:
         raise ValueError("Содержимое аргумента, переданного в функцию define_winner_hand не корректно!")
 
-    # Список всех возможных 5-карточных комбинаций в руке
     all_combo_in_hand = list(combinations(scs.list_of_cards, 5))
 
-    # Сортировка по масти
-    ordered_all_combo_in_hand_by_suits = []
+    ordered_all_combo_in_hand_by_suits = [sorted(hnd, key=lambda crd: crd[1]) for hnd in all_combo_in_hand]
 
-    for hnd in all_combo_in_hand:
-        shnd = sorted(hnd, key=lambda crd: crd[1])
-        ordered_all_combo_in_hand_by_suits.append(shnd)
+    hands_scores = score_for_a_combination(ordered_all_combo_in_hand_by_suits)
 
-    hands_scores = scoring_for_a_combination(ordered_all_combo_in_hand_by_suits)
-
-    # Найти максимум в полученном списке оцененных рук и количество рук с максимумом очков
     max_score = max(hands_scores)
 
-    ind_max = []
-    count_ind = 0
-    for s in hands_scores:
-        if s == max_score:
-            ind_max.append(count_ind)
-        count_ind += 1
+    ind_max = [i for i, s in enumerate(hands_scores) if s == max_score]
 
-    # Собираем список рук с выигрышными по баллу комбинациями и сортируем по номиналу
-    hand_max = []
-    for i in ind_max:
-        hand_max.append(sorted(all_combo_in_hand[i], key=lambda crd: crd[0]))
+    hand_max = [sorted(all_combo_in_hand[i], key=lambda crd: crd[0]) for i in ind_max]
 
-    # Определяем итоговую выигрышную комбинацию
     if len(hand_max) == 1:
         win_hand = hand_max[0]
-
     else:
         win_hand = compare_of_equal_combos(hand_max, max_score)
 
     return win_hand, max_score
 
 
-def scoring_for_a_combination(ordered_acih_by_suits: list) -> list:
+def score_combo_with_same_suit(shnd: list) -> int:
+    shnd = sorted(shnd, key=lambda crd: crd[0])
+    if shnd[4][0] - shnd[0][0] == 4:
+        if shnd[0][0] == 10:
+            return 10  # Royal flush
+        else:
+            return 9  # Straight flush
+    else:
+        if 14 in shnd and shnd[3][0] - shnd[0][0] == 3 and shnd[0] == 2:
+            return 5  # Straight
+        else:
+            return 6  # Flush
+
+
+def score_combo_without_same_suit(shnd: list) -> int:
+    shnd = sorted(shnd, key=lambda crd: crd[0])
+    sorted_list_of_values = [shnd[n][0] for n in range(0, 5)]
+    ace = 14
+
+    if sorted_list_of_values[4] - sorted_list_of_values[0] == 4 and len(sorted_list_of_values) == len(set(sorted_list_of_values)):
+        return 5  # Straight
+    elif ace in sorted_list_of_values and sorted_list_of_values[3] - sorted_list_of_values[0] == 3 and sorted_list_of_values[0] == 2:
+        return 5  # Straight
+    else:
+        counter_values = Counter(sorted_list_of_values)
+        values = list(counter_values.values())
+
+        if 4 in values:
+            return 8  # Four of a kind
+        elif 3 in values:
+            if 2 in values:
+                return 7  # Full house
+            else:
+                return 4  # Three of a kind
+        elif 2 in values:
+            vc = values.count(2)
+            if vc == 2:
+                return 3  # Two pairs
+            else:
+                return 2  # Pair
+        else:
+            return 1  # Highcard
+
+
+def score_for_a_combination(ordered_acih_by_suits: list) -> list:
     if not isinstance(ordered_acih_by_suits, list):
         raise TypeError("В функцию scoring_for_a_combination передан аргумент некорректного типа!")
     if not ordered_acih_by_suits:
         raise ValueError("Содержимое аргумента, переданного в функцию scoring_for_a_combination не корректно!")
 
-    hands_scores = []
-
-    for shnd in ordered_acih_by_suits:
-        if shnd[0][1] == shnd[4][1]:
-            shnd = sorted(shnd, key=lambda crd: crd[0])
-            if shnd[4][0] - shnd[0][0] == 4:
-                if shnd[0][0] == 10:
-                    hands_scores.append(10)  # Royal flush
-                else:
-                    hands_scores.append(9)  # Straight flush
-            else:
-                if 14 in shnd and shnd[3][0] - shnd[0][0] == 3 and shnd[0] == 2:
-                    hands_scores.append(5)  # Straight
-                else:
-                    hands_scores.append(6)  # Flush
-
-        else:
-            shnd = sorted(shnd, key=lambda crd: crd[0])
-            sorted_list_of_values = []
-            for n in range(0, 5):
-                sorted_list_of_values.append(shnd[n][0])
-
-            if sorted_list_of_values[4] - sorted_list_of_values[0] == 4 \
-                    and len(sorted_list_of_values) == len(set(sorted_list_of_values)):
-                hands_scores.append(5)  # Straight
-                continue
-
-            elif 14 in sorted_list_of_values and sorted_list_of_values[3] - sorted_list_of_values[0] == 3 \
-                    and sorted_list_of_values[0] == 2:
-                hands_scores.append(5)  # Straight
-                continue
-
-            else:
-                counter_values = Counter(sorted_list_of_values)
-                values = list(counter_values.values())
-
-                if 4 in values:
-                    hands_scores.append(8)  # Four of a kind
-                    continue
-
-                elif 3 in values:
-                    if 2 in values:
-                        hands_scores.append(7)  # Full house
-                        continue
-                    else:
-                        hands_scores.append(4)  # Three of a kind
-                        continue
-
-                elif 2 in values:
-                    vc = values.count(2)
-                    if vc == 2:
-                        hands_scores.append(3)  # Two pairs
-                        continue
-                    else:
-                        hands_scores.append(2)  # Pair
-                        continue
-                else:
-                    hands_scores.append(1)  # Highcard
+    hands_scores = [score_combo_with_same_suit(shnd) if shnd[0][1] == shnd[4][1] else score_combo_without_same_suit(shnd) for shnd in ordered_acih_by_suits]
     return hands_scores
 
 
@@ -135,10 +141,7 @@ def clean_of_card_suit(hand_max: list) -> list:
     if not hand_max:
         raise ValueError("Содержимое аргумента, переданного в функцию clean_of_card_suit не корректно!")
 
-    hand_max_without_suit = []
-    for hand in hand_max:
-        hand_max_without_suit.append([hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]])
-    return hand_max_without_suit
+    return [[hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]] for hand in hand_max]
 
 
 def find_win_hand(hand_max_without_suit: list, hand_max: list, max_score: int) -> list:
@@ -151,10 +154,9 @@ def find_win_hand(hand_max_without_suit: list, hand_max: list, max_score: int) -
         winner = 0
         winner_hand_without_suit = []
         for hand in hand_max_without_suit:
-            if hand[4] > winner:
-                winner = hand[4]
+            if hand[-1] > winner:
+                winner = hand[-1]
                 winner_hand_without_suit = hand
-
         windex = hand_max_without_suit.index(winner_hand_without_suit)
         win_hand = hand_max[windex]
 
@@ -163,6 +165,35 @@ def find_win_hand(hand_max_without_suit: list, hand_max: list, max_score: int) -
         windex = hand_max_without_suit.index(winner_hand_without_suit)
         win_hand = hand_max[windex]
 
+    return win_hand
+
+
+def change_for_rare_case(hand_max_without_suit: list) -> list:
+    for hand in hand_max_without_suit:
+        if 14 in hand and 2 in hand:
+            hand.insert(0, 14)
+            hand.pop(5)
+    return hand_max_without_suit
+
+
+def change_of_a_kind(hand_max_without_suit: list) -> list:
+    for hand in hand_max_without_suit:
+        value_of_the_same = hand[2]
+        ind = hand_max_without_suit.index(hand)
+        for i in range(3):
+            hand_max_without_suit[ind].remove(value_of_the_same)
+    return hand_max_without_suit
+
+
+def check_equals_for_fullhouse(hand_max_without_suit: list, hand_max: list) -> list:
+    nominals = [[hand[0], hand[4]] if hand.count(hand[0]) == 3 else [hand[4], hand[0]] for hand in
+                hand_max_without_suit]
+    tmp_three = [n[0] for n in nominals]
+    max_of_three = max(tmp_three)
+    tmp_hand = [nominals[i] for i in range(len(nominals)) if nominals[i][0] == max_of_three]
+    count_list = [i for i, s in enumerate(nominals) if nominals[i][0] == max_of_three]
+    win_hand = [hand_max[count_list[i]] if tmp_hand[i][1] >= tmp_hand[i + 1][1] else hand_max[count_list[i + 1]] for
+                i in range(len(tmp_hand) - 1)]
     return win_hand
 
 
@@ -179,57 +210,17 @@ def compare_of_equal_combos(hand_max: list, max_score: int) -> list:
 
     if max_score == 9:
         hand_max_without_suit = clean_of_card_suit(hand_max)
-
-        for hand in hand_max_without_suit:
-            if 14 in hand and 2 in hand:
-                hand.insert(0, 14)
-                hand.pop(5)
-
+        hand_max_without_suit = change_for_rare_case(hand_max_without_suit)
         win_hand = find_win_hand(hand_max_without_suit, hand_max, max_score)
 
     if max_score == 8:
         hand_max_without_suit = clean_of_card_suit(hand_max)
-
-        for hand in hand_max_without_suit:
-            value_of_four = hand[2]
-            ind = hand_max_without_suit.index(hand)
-            for i in range(3):
-                hand_max_without_suit[ind].remove(value_of_four)
-
+        hand_max_without_suit = change_of_a_kind(hand_max_without_suit)
         win_hand = find_win_hand(hand_max_without_suit, hand_max, max_score)
 
     if max_score == 7:
         hand_max_without_suit = clean_of_card_suit(hand_max)
-
-        nominals = []
-        for hand in hand_max_without_suit:
-            count_start = hand.count(hand[0])
-            if count_start == 3:
-                nominals.append([hand[0], hand[4]])
-            else:
-                nominals.append([hand[4], hand[0]])
-
-        tmp_three = []
-        for n in nominals:
-            tmp_three.append(n[0])
-
-        max_of_three = max(tmp_three)
-
-        tmp_hand = []
-        count_list = []
-        count = 0
-        for i in range(len(nominals)):
-            if nominals[i][0] == max_of_three:
-                tmp_hand.append(nominals[i])
-                count_list.append(count)
-            count += 1
-
-        win_hand = []
-        for i in range(len(tmp_hand) - 1):
-            if tmp_hand[i][1] >= tmp_hand[i + 1][1]:
-                win_hand = hand_max[count_list[i]]
-            else:
-                win_hand = hand_max[count_list[i + 1]]
+        win_hand = check_equals_for_fullhouse(hand_max_without_suit, hand_max)
 
     if max_score == 6:
         hand_max_without_suit = clean_of_card_suit(hand_max)
@@ -237,23 +228,12 @@ def compare_of_equal_combos(hand_max: list, max_score: int) -> list:
 
     if max_score == 5:
         hand_max_without_suit = clean_of_card_suit(hand_max)
-
-        for hand in hand_max_without_suit:
-            if 14 in hand and 2 in hand:
-                hand.insert(0, 14)
-                hand.pop(5)
-
+        hand_max_without_suit = change_for_rare_case(hand_max_without_suit)
         win_hand = find_win_hand(hand_max_without_suit, hand_max, max_score)
 
     if max_score == 4:
         hand_max_without_suit = clean_of_card_suit(hand_max)
-
-        for hand in hand_max_without_suit:
-            value_of_three = hand[2]
-            ind = hand_max_without_suit.index(hand)
-            for i in range(3):
-                hand_max_without_suit[ind].remove(value_of_three)
-
+        hand_max_without_suit = change_of_a_kind(hand_max_without_suit)
         win_hand = find_win_hand(hand_max_without_suit, hand_max, max_score)
 
     if max_score == 3:
@@ -271,8 +251,8 @@ def compare_of_equal_combos(hand_max: list, max_score: int) -> list:
     return win_hand
 
 
-all_cards = tuple([(x, y) for x in range(2, 15) for y in ('H', 'S', 'C', 'D')])
-kit_cards = random.sample(all_cards, 7)
+# all_cards = tuple([(x, y) for x in range(2, 15) for y in ('H', 'S', 'C', 'D')])
+# kit_cards = random.sample(all_cards, 7)
 
 # kit_cards = [(12, 'D'), (13, 'S'), (7, 'S'), (11, 'S'), (14, 'D'), (6, 'C'), (4, 'D')]  # Highcard
 # kit_cards = [(5, 'S'), (4, 'D'), (6, 'D'), (12, 'C'), (4, 'S'), (7, 'D'), (2, 'D')]  # Pair
@@ -286,9 +266,11 @@ kit_cards = random.sample(all_cards, 7)
 # kit_cards = [(2, 'H'), (6, 'S'), (6, 'C'), (6, 'H'), (6, 'D'), (4, 'S'), (8, 'C')]  # Four of a kind
 # kit_cards = [(2, 'H'), (3, 'H'), (4, 'H'), (5, 'H'), (6, 'H'), (7, 'H'), (8, 'H')]  # Straight flush
 # kit_cards = [(4, 'H'), (3, 'S'), (10, 'H'), (11, 'H'), (12, 'H'), (13, 'H'), (14, 'H')]  # Royal flush
-
+# kit_cards = [(2, 'S'), (4, 'H'), (6, 'H'), (8, 'H'), (10, 'H'), (12, 'H'), (14, 'C')]
+# kit_cards = [(2, 'H'), (4, 'C'), (6, 'C'), (8, 'C'), (10, 'C'), (12, 'H'), (14, 'C')]
 # kit_cards = []
 # seven_cards = []
+
 seven_cards = SevenCards(kit_cards)
 
 combo_names = {1: 'Highcard', 2: 'Pair', 3: 'Two pairs', 4: 'Three of a kind', 5: 'Straight',
